ab22da515427f6f7909e5ffeb4a3d16d3dcb373b
Your Name <you@example.com>
2025-02-20 11:05:53 +0100

    February 20th, update from Semia

---
+++ src/models/Content.js
@@ -0,0 +1,24 @@
+// src/models/Content.js
+module.exports = (sequelize, DataTypes) => {
+  const Content = sequelize.define('Content', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    content_en: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    content_fr: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+
+  return Content;
+};
--- src/models/index.js
+++ src/models/index.js
@@ -5,11 +5,13 @@ const { DataTypes } = require('sequelize');
 // Importation de ton modèle Testimonial
 const Testimonial = require('./Testimonial')(sequelize, DataTypes);
 const User = require('./User')(sequelize, DataTypes);
+const Content = require('./Content')(sequelize, DataTypes);
 
 
 // Exportation des modèles pour les utiliser dans toute l'application
 module.exports = {
   sequelize,
   Testimonial,
+  Content,
   User
 };
